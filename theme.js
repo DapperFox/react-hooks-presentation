cde7265458e600c73b233d5e92d0a6c48cfb6a9f
Leah Loughran <leahloughran@gmail.com>
2019-06-26 10:42:32 -0600

    update

--- theme.js
+++ theme.js
@@ -1,13 +1,16 @@
-import { swiss as baseTheme } from 'mdx-deck/themes';
+import { dark as baseTheme } from 'mdx-deck/themes';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import { javascript } from 'react-syntax-highlighter/dist/esm/languages/prism'
 
-export default {
+export const kDark = {
   ...baseTheme,
   prism: {
     style: dark,
     languages: {
       javascript: javascript
     }
+  },
+  li: {
+    padding: '15px'
   }
 }
